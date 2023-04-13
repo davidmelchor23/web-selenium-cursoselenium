294f7d70fef52b6481635c1cb1c983961f30c7a1
davidmelchor23 <david.melchor@bowpi.com>
2023-04-13 10:14:50 -0600

    Operadores

---
+++ SeleniumAutomation/src/test/java/com/javabasic/Datatypes.java
@@ -0,0 +1,54 @@
+package com.javabasic;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/* Datos Numericos */
+		
+//		byte x;
+//		short y;
+//		int z;
+//		long a;
+//		
+//		x = 127;
+//		y = 32767;
+//		z = 2147483647;
+//		a = 9223372036854775807L;
+//		
+//		System.out.println(x);
+		
+		/* Datos Numericos */
+		
+//		float base;
+//		double altura;
+//		
+//		base = 30.5F;
+//		altura = 40.76;
+//		
+//		System.out.println(base);
+		
+		/* Caracteres */
+		
+//		char letra1;
+//		char letra2;
+//		String frase;
+//		
+//		letra1 = 'a';
+//		letra2 = 'x';
+//		frase = "hola";
+//		
+//		System.out.println(frase);
+		
+		/* Booleanos */
+		
+		boolean avesVuelan;
+		
+		avesVuelan = true;
+		
+		System.out.println("Las aves vuelan? " + avesVuelan);
+		
+	}
+
+}
---
+++ SeleniumAutomation/src/test/java/com/javabasic/OperatorStatements.java
@@ -0,0 +1,45 @@
+package com.javabasic;
+
+public class OperatorStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/* Operadores Aritmeticos */
+		
+//		int x = 1;
+//		int y = 2;
+//		
+//		int suma;
+//		
+//		suma = x + y;
+//		
+//		System.out.println(suma);
+		
+		/*
+		 * Operadores Unarios
+		 */
+		
+//		int valor1 = 3;
+//		int incrementar = ++valor1;
+//	
+//		System.out.println(incrementar);
+		
+		/*
+		 * Operadores Relacionales
+		 */
+		
+		int num1 = 10;
+		int num2 = 5;
+		int num3 = 6;
+		int num4 = 11;
+		
+		if (num1 >= 10 || num2 > 10) {
+			System.out.println("Los numeros son mayores a 10");			
+		}else {
+			System.out.println("Uno de los dos numeros o los dos numeros son menores a 10");
+		}
+			
+	}
+
+}
